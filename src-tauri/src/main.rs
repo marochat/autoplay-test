27e0e7919d1638f54714bc85d2b228e81097505a
marochat <mamiyanx@gmail.com>
2022-11-27 17:32:41 +0900

    insert invoke function

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,9 +8,14 @@ fn console_out(mes: String) {
   println!("{}", mes);
 }
 
+#[tauri::command]
+fn void_func() {
+
+}
+
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![ console_out ])
+    .invoke_handler(tauri::generate_handler![ console_out, void_func ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/index.js
+++ src/index.js
@@ -14,11 +14,14 @@ const logging = (mes) => {
 /**
  * start after window loaded.
  */
-new Promise(resolv => window.onload = () => resolv())
-.then(() => {
+new Promise(resolv => window.onload = resolv()).then(() => {
+  invoke('void_func').then(() => {
+    new Audio(chimeSound).play();
+  });
   const btn1 = document.getElementById('btn1');
   const btn2 = document.getElementById('btn2');
   const btn3 = document.getElementById('btn3');
+  const btn4 = document.getElementById('btn4');
 
   /** playing sample1 audio -- with user interactive */
   btn1.onclick = () => {
@@ -45,7 +48,10 @@ new Promise(resolv => window.onload = () => resolv())
   };
 
   /** playing sample2 audio -- pre-create AudioContext non-interactive */
-  const ctx_pre = new AudioContext();
+  let ctx_pre;
+  invoke('void_func').then(() => {
+    ctx_pre = new AudioContext();
+  })
   btn2.onclick = () => {
     // const ctx = new AudioContext();
     const source = ctx_pre.createBufferSource();
@@ -72,26 +78,41 @@ new Promise(resolv => window.onload = () => resolv())
   /** playing sample3 audio -- 1s timer play == autoplay test */
   btn3.onclick = () => {
     setTimeout(() => {
-      const ctx = new AudioContext();
-      const source = ctx.createBufferSource();
-      fetch(chimeSound)
-      .then(
-        res => res.arrayBuffer()
-      )
-      .then(
-        arryb => ctx.decodeAudioData(arryb)
-      )
-      .then(
-        adbuf => {
-          source.buffer = adbuf;
-          source.connect(ctx.destination);
-          source.onended = () => {
-            logging('sample3 ended.');
+      invoke('void_func').then(() => {
+        const ctx = new AudioContext();
+        const source = ctx.createBufferSource();
+        fetch(chimeSound)
+        .then(
+          res => res.arrayBuffer()
+        )
+        .then(
+          arryb => ctx.decodeAudioData(arryb)
+        )
+        .then(
+          adbuf => {
+            source.buffer = adbuf;
+            source.connect(ctx.destination);
+            source.onended = () => {
+              logging('sample3 ended.');
+            }
+            logging('sample3 started.')
+            source.start(0);
           }
-          logging('sample3 started.')
-          source.start(0);
-        }
-      )
+        )        
+      });
     }, 1000)
   }
+  /** playing sample4 */
+  const atag = document.querySelector('audio');
+  //const asrc = ddocument.querySelector('source');
+  atag.src = chimeSound;
+  atag.onended = () => logging('sample4 ended.');
+  btn4.onclick = () => {
+    //atag.play();
+    setTimeout(() => {
+      logging('sample4 started.')
+      atag.play();  
+    }, 1000)
+  }
+  
 });
